95c19c60e0feb4428bc22f3b1554e0b1fb458830
jordibenck <jordi.b-98@hotmail.com>
2016-09-25 11:33:09 +0200

    Assignment done

---
+++ CommandingAndBindingDemo/DegreesToFahrenheitConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CommandingAndBindingDemo
+{
+    class DegreesToFahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double result = (double)value;
+            return (result * 1.8) + 32;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //De convert back is onnodig. 
+            return value;
+        }
+    }
+}
